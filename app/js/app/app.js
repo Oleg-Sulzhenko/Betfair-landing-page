dc2d233623f4cfcd5915c167fefa7a73e37e87be
Oleg Sulzhenko <webaleg@gmail.com>
2014-08-21 16:19:03 +0300

    Done and tested

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -7,32 +7,29 @@ transformEventName = Modernizr.prefixed('transform'),
         transEventName = Modernizr.prefixed('transition'),
         transEndEventName = transEndEventNames[transEventName];
 
-// identify browser
-var isMobileDevice = false;
-document.isMobileDevice = isMobileDevice;
-
-if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
-    document.documentElement.classList.add('ios');
-    document.isMobileDevice = true;
-}
-;
-if (navigator.userAgent.match(/Android/i)) {
-    document.documentElement.classList.add('android');
-    document.isMobileDevice = true;
-}
-if (document.all && document.querySelector && !document.addEventListener) {
-    document.documentElement.classList.add('ie8');
-}
 // fadeIn page (load or DOMContentLoaded);
 window.addEventListener && window.addEventListener('load', function() {
     document.body.className += ' loaded';
 });
 
-
-
+// identify browser
+var isMobileDevice = false;
+var isIE = false;
+if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
+    document.documentElement.classList.add('ios');
+    isMobileDevice = true;
+}
+if (navigator.userAgent.match(/Android/i)) {
+    document.documentElement.classList.add('android');
+    isMobileDevice = true;
+}
+if (document.all && document.querySelector && !document.addEventListener) {
+    document.documentElement.classList.add('ie8');
+    isIE = true;
+}
 
 //User bandwidth speed detect
-var imageAddr = "../../img/test.jpg" + "?n=" + Math.random(); // size 761441 bytes
+var imageAddr = "img/test.jpg" + "?n=" + Math.random(); // size 761441 bytes
 var startTime, endTime;
 var downloadSize = 761441;
 var download = new Image();
@@ -51,123 +48,80 @@ function showResults() {
     var minBandwidth = 2;
 
     console.log("Your connection speed is: \n" + speedMbps + " Mbps\n");
-    
+
     if (speedMbps > minBandwidth && !(isMobileDevice)) {
+        //Start downloading Vimeo iframe video content
         $("#video-iframe").show();
         $("#video-iframe").show(function() {
             $(this).prop("src", function() {
                 return $(this).data("src");
             });
         });
-
-
-        //------------------------
-
-
-
-
-
-
+        
         //Vimeo player API, mute video on start
-        $(function() {
-            var player = $('#video-iframe');
-            var url = window.location.protocol + player.attr('src').split('?')[0];
-            // Listen for messages from the player
-            if (window.addEventListener) {
-                window.addEventListener('message', onMessageReceived, false);
-            }
-            else {
-                window.attachEvent('onmessage', onMessageReceived, false);
-            }
-            // Handle messages received from the player
-            function onMessageReceived(e) {
-                var data = JSON.parse(e.data);
-                switch (data.event) {
-                    case 'ready':
-                        onReady();
-                        break;
+        //do not Mute on ie8 (its too buggy)
+        if (!(isIE)) {
+            $(function() {
+                var player = $('#video-iframe');
+                var url = window.location.protocol + player.attr('src').split('?')[0];
+                // Listen for messages from the player
+                if (window.addEventListener) {
+                    window.addEventListener('message', onMessageReceived, false);
                 }
-            }
-            // Helper function for sending a message to the player
-            function post(action, value) {
-                var data = {
-                    method: action
-                };
-                if (value) {
-                    data.value = value;
+                else {
+                    window.attachEvent('onmessage', onMessageReceived, false);
                 }
-                var message = JSON.stringify(data);
-                player[0].contentWindow.postMessage(data, url);
-            }
-            function onReady() {
-                console.log('ready');
-                post('setVolume', '0');
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //-------------------------------------------------------------
-
+                // Handle messages received from the player
+                function onMessageReceived(e) {
+                    onReady();
+                }
+                // Helper function for sending a message to the player
+                function post(action, value) {
+                    var data = {
+                        method: action
+                    };
+                    if (value) {
+                        data.value = value;
+                    }
+                    var message = JSON.stringify(data);
+                    player[0].contentWindow.postMessage(data, url);
+                }
+                function onReady() {
+                    post('setVolume', '0');
+                }
+            });
+        }
 
     } else {
         $("#video-backup-img").show();
     }
 }
 
-//Scrol Effects
-var fadeUntil = 500;
-var fadeUntilText = 450;
-var fading = $('.bg');
-var fadingText = $('.main-cta');
-$(window).bind('scroll', function() {
-    var offset = $(document).scrollTop();
-    if (offset <= fadeUntil && !(isMobileDevice)) {
-        fading.removeClass("fade-complete");
-        var opacity = 0;
-        opacity = offset / fadeUntil;
-    } else {
-        fading.addClass("fade-complete");
-    }
-    fading.css('opacity', opacity);
+if (!(isMobileDevice)) {
+    //Scrol Effects
+    var fadeUntil = 500;
+    var fadeUntilText = 300;
+    var fading = $('.bg');
+    var fadingText = $('.main-cta');
+    $(window).bind('scroll', function() {
+        var offset = $(document).scrollTop();
+        if (offset <= fadeUntil && !(isMobileDevice)) {
+            fading.removeClass("fade-complete");
+            var opacity = 0;
+            opacity = offset / fadeUntil;
+        } else {
+            fading.addClass("fade-complete");
+        }
+        fading.css('opacity', opacity);
 
+        if (offset <= fadeUntilText && !(isMobileDevice)) {
+            fadingText.removeClass("fade-complete");
+            var top = 160;
+            top += (offset / fadeUntilText) * 200;
+        } else {
+            fadingText.addClass("fade-complete");
+        }
+        fadingText.css('top', top);
+    });
+}
 
-    if (offset <= fadeUntilText && !(isMobileDevice)) {
-        fadingText.removeClass("fade-complete");
-        var top = 160;
-        top += (offset / fadeUntilText) * 400;
-    } else {
-        fadingText.addClass("fade-complete");
-    }
-    fadingText.css('top', top);
-});
