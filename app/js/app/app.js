e0825cca5bd73fb97b0a6b4d475d4da0227ad299
Oleg Sulzhenko <webaleg@gmail.com>
2014-08-21 11:50:53 +0300

    before upload to server

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -1,30 +1,31 @@
 var transEndEventNames = {
-		'WebkitTransition': 'webkitTransitionEnd', // Saf 6, Android Browser
-		'MozTransition': 'transitionend', // only for FF < 15
-		'transition': 'transitionend' // IE10, Opera, Chrome, FF 15+, Saf 7+
-	},
-	transformEventName = Modernizr.prefixed('transform'),
-	transEventName = Modernizr.prefixed('transition'),
-	transEndEventName = transEndEventNames[transEventName];
+    'WebkitTransition': 'webkitTransitionEnd', // Saf 6, Android Browser
+    'MozTransition': 'transitionend', // only for FF < 15
+    'transition': 'transitionend' // IE10, Opera, Chrome, FF 15+, Saf 7+
+},
+transformEventName = Modernizr.prefixed('transform'),
+        transEventName = Modernizr.prefixed('transition'),
+        transEndEventName = transEndEventNames[transEventName];
 
 // identify browser
 var isMobileDevice = false;
 document.isMobileDevice = isMobileDevice;
 
 if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g)) {
-	document.documentElement.classList.add('ios');
-        document.isMobileDevice = true;
-};
+    document.documentElement.classList.add('ios');
+    document.isMobileDevice = true;
+}
+;
 if (navigator.userAgent.match(/Android/i)) {
-	document.documentElement.classList.add('android');
-        document.isMobileDevice = true;
+    document.documentElement.classList.add('android');
+    document.isMobileDevice = true;
 }
 if (document.all && document.querySelector && !document.addEventListener) {
-	document.documentElement.classList.add('ie8');
+    document.documentElement.classList.add('ie8');
 }
 // fadeIn page (load or DOMContentLoaded);
-window.addEventListener && window.addEventListener('load', function () {
-	document.body.className += ' loaded';
+window.addEventListener && window.addEventListener('load', function() {
+    document.body.className += ' loaded';
 });
 
 
@@ -35,32 +36,113 @@ var imageAddr = "../../img/test.jpg" + "?n=" + Math.random(); // size 761441 byt
 var startTime, endTime;
 var downloadSize = 761441;
 var download = new Image();
-download.onload = function () {
-	endTime = (new Date()).getTime();
-	showResults();
+download.onload = function() {
+    endTime = (new Date()).getTime();
+    showResults();
 }
 startTime = (new Date()).getTime();
 download.src = imageAddr;
 function showResults() {
-	var duration = (endTime - startTime) / 1000;
-	var bitsLoaded = downloadSize * 8;
-	var speedBps = (bitsLoaded / duration).toFixed(2);
-	var speedKbps = (speedBps / 1024).toFixed(2);
-	var speedMbps = (speedKbps / 1024).toFixed(2);
-	var minBandwidth = 2;
+    var duration = (endTime - startTime) / 1000;
+    var bitsLoaded = downloadSize * 8;
+    var speedBps = (bitsLoaded / duration).toFixed(2);
+    var speedKbps = (speedBps / 1024).toFixed(2);
+    var speedMbps = (speedKbps / 1024).toFixed(2);
+    var minBandwidth = 2;
 
-	console.log("Your connection speed is: \n" + speedMbps + " Mbps\n");
-        console.log(isMobileDevice);
-	if(speedMbps > minBandwidth && !(isMobileDevice)){
-            $("#video-iframe").show();
-            $("#video-iframe").show(function(){
-                    $(this).prop("src", function(){
-                            return $(this).data("src");
-                    });
+    console.log("Your connection speed is: \n" + speedMbps + " Mbps\n");
+    
+    if (speedMbps > minBandwidth && !(isMobileDevice)) {
+        $("#video-iframe").show();
+        $("#video-iframe").show(function() {
+            $(this).prop("src", function() {
+                return $(this).data("src");
             });
-	} else {
-            $("#video-backup-img").show();
-	}
+        });
+
+
+        //------------------------
+
+
+
+
+
+
+        //Vimeo player API, mute video on start
+        $(function() {
+            var player = $('#video-iframe');
+            var url = window.location.protocol + player.attr('src').split('?')[0];
+            // Listen for messages from the player
+            if (window.addEventListener) {
+                window.addEventListener('message', onMessageReceived, false);
+            }
+            else {
+                window.attachEvent('onmessage', onMessageReceived, false);
+            }
+            // Handle messages received from the player
+            function onMessageReceived(e) {
+                var data = JSON.parse(e.data);
+                switch (data.event) {
+                    case 'ready':
+                        onReady();
+                        break;
+                }
+            }
+            // Helper function for sending a message to the player
+            function post(action, value) {
+                var data = {
+                    method: action
+                };
+                if (value) {
+                    data.value = value;
+                }
+                var message = JSON.stringify(data);
+                player[0].contentWindow.postMessage(data, url);
+            }
+            function onReady() {
+                console.log('ready');
+                post('setVolume', '0');
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //-------------------------------------------------------------
+
+
+    } else {
+        $("#video-backup-img").show();
+    }
 }
 
 //Scrol Effects
@@ -68,24 +150,24 @@ var fadeUntil = 500;
 var fadeUntilText = 450;
 var fading = $('.bg');
 var fadingText = $('.main-cta');
-$(window).bind('scroll', function(){
+$(window).bind('scroll', function() {
     var offset = $(document).scrollTop();
-    if( offset<=fadeUntil && !(isMobileDevice) ){
+    if (offset <= fadeUntil && !(isMobileDevice)) {
         fading.removeClass("fade-complete");
         var opacity = 0;
-        opacity = offset/fadeUntil;
+        opacity = offset / fadeUntil;
     } else {
         fading.addClass("fade-complete");
     }
-    fading.css('opacity',opacity);
-    
-    
-    if( offset<=fadeUntilText && !(isMobileDevice) ){
+    fading.css('opacity', opacity);
+
+
+    if (offset <= fadeUntilText && !(isMobileDevice)) {
         fadingText.removeClass("fade-complete");
         var top = 160;
-        top += (offset/fadeUntilText)*400;
+        top += (offset / fadeUntilText) * 400;
     } else {
         fadingText.addClass("fade-complete");
     }
-    fadingText.css('top',top);
+    fadingText.css('top', top);
 });
